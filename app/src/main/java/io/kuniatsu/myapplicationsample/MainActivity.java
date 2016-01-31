7ddb268dd33a9f3727d5ec0e79525371c2d38cf8
kuniatus <nightmaredes@hotmail.com>
2016-01-31 17:32:57 +0900

    Initial commit

--- app/src/main/java/io/kuniatsu/myapplicationsample/MainActivity.java
+++ app/src/main/java/io/kuniatsu/myapplicationsample/MainActivity.java
@@ -24,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                /*
+
+                ここからはじまるマイレボリューション
+
+
+
+                 */
+
+
+
+
             }
         });
     }
